abd75d2e9d1e4a8187c8c8797d9c4a016fcb8600
Nathan Croawell <nathancroawell@gmail.com>
2021-01-05 20:59:25 +0000

    initialising a new vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header title="Things I found on the internet"></the-header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TheHeader,
   }
 }
 </script>
 
 <style>
-#app {
+* {
+  box-sizing: border-box;
+}
+
+html {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
 }
 </style>
